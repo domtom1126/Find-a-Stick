1f16c214f6d51aefd0691095ccf179a8a3138d17
dom thomas <domtom1126@gmail.com>
2021-11-01 12:49:14 -0400

    functionality not there, screen for user liked cars

--- lib/screens/liked_screen.dart
+++ lib/screens/liked_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:find_stick/widgets/image_gallery.dart';
 import 'package:flutter/material.dart';
 
 class LikedScreen extends StatefulWidget {
@@ -8,10 +10,19 @@ class LikedScreen extends StatefulWidget {
 }
 
 class _LikedScreenState extends State<LikedScreen> {
+  final imageList = [
+    'http://wallup.net/wp-content/uploads/2016/01/272941-Dodge_Viper-car-sports_car-orange.jpg',
+    'https://pictures.topspeed.com/IMG/crop/202011/cool-car-for-sale-20-5_1600x0w.jpg',
+    'https://wallup.net/wp-content/uploads/2016/01/136864-orange_cars-car-BMW.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Liked page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Liked'),
+      ),
+      // Todo This is where the liked widget should go
+      body: null,
     );
   }
 }
