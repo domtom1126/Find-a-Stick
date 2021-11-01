c67ca0b1ad231f11f71c7e6a51f3369c3c187412
dom thomas <domtom1126@gmail.com>
2021-11-01 13:13:08 -0400

    user settings

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,23 @@
+// page for user Settings
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Text('Settings'),
+      ),
+    );
+  }
+}
