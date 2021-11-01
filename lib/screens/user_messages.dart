dd54f5eb24dc3eed6744eb17772f91f2abcd9019
dom thomas <domtom1126@gmail.com>
2021-11-01 13:13:40 -0400

    messages between users

---
+++ lib/screens/user_messages.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class UserMessages extends StatefulWidget {
+  const UserMessages({Key? key}) : super(key: key);
+
+  @override
+  _UserMessagesState createState() => _UserMessagesState();
+}
+
+class _UserMessagesState extends State<UserMessages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Messages'),
+      ),
+      body: Center(
+        child: Text('Messages'),
+      ),
+    );
+  }
+}
