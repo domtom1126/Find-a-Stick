57741e9d789d0b4840c6f778d01ddd1ebc32b735
dom thomas <domtom1126@gmail.com>
2021-12-06 02:35:53 -0500

    added search icon in appbar

--- lib/widgets/list_view.dart
+++ lib/widgets/list_view.dart
@@ -2,7 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:find_stick/screens/each_car.dart';
 import 'package:find_stick/screens/user_messages.dart';
-import 'package:find_stick/widgets/image_gallery.dart';
+// import 'package:find_stick/widgets/image_gallery.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
