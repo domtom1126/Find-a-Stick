77b9ccd717f71479bce5e7ea8972d107c2bd330c
dom thomas <domtom1126@gmail.com>
2022-01-05 23:22:16 -0500

    Seperate screen for each listing

--- lib/screens/each_car.dart
+++ lib/screens/each_car.dart
@@ -1,17 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:find_stick/screens/user_messages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
-class SingleListingInfo extends StatelessWidget {
+class SingleListingInfo extends StatefulWidget {
   var make;
   var model;
   var year;
   var trim;
   var miles;
+  var price;
+  var description;
 
   SingleListingInfo(
-      {Key? key, this.make, this.model, this.year, this.trim, this.miles})
+      {Key? key,
+      this.make,
+      this.model,
+      this.year,
+      this.trim,
+      this.miles,
+      this.price,
+      this.description})
       : super(key: key);
+
+  @override
+  State<SingleListingInfo> createState() => _SingleListingInfoState();
+}
+
+class _SingleListingInfoState extends State<SingleListingInfo> {
+  List<bool> _likes = List.filled(1, true);
 
   @override
   Widget build(BuildContext context) {
@@ -19,67 +38,106 @@ class SingleListingInfo extends StatelessWidget {
         appBar: AppBar(
           title: Text('Listing Info'),
         ),
-        body: Container(
-            child: Column(
-          children: <Widget>[
-            Placeholder(fallbackHeight: 100),
-            ListTile(
-              title: Text('$year $make $model $trim'),
-              subtitle: Text('$miles Miles'),
-            ),
-            Expanded(
-              child: Divider(
-                color: Colors.blueGrey,
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: // on pressed show bottom modal for 10 seconds that shows you added to liked List
-                        () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return Container(
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Text(
-                                    'Added to Liked List',
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  SizedBox(height: 25),
-                                  // add button to remove from liked list
-                                  CupertinoButton(
-                                    color: Colors.red,
-                                    onPressed: () {},
-                                    child: Text('Remove from list'),
-                                  ),
-                                  SizedBox(height: 25),
-                                ],
-                              ),
-                            );
-                          });
-                    },
-                    child: Text('Like'),
+        body: ListView(
+          children: [
+            Column(
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.all(10),
+                    child: Placeholder(fallbackHeight: 250)),
+
+                Container(
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.only(left: 10, top: 10),
+                    child: Text(
+                      '${widget.price}',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
+                Container(
+                  margin: EdgeInsets.only(top: 0, bottom: 15),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.only(left: 10),
+                    title: Text(
+                      '${widget.year} ${widget.make} ${widget.model} ${widget.trim}',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text('${widget.miles} Miles',
+                        style: TextStyle(fontSize: 15)),
+                    trailing: // Like button to change color on click
+                        IconButton(
+                      iconSize: 30,
+                      icon: Icon(Icons.favorite),
+                      color: _likes[0] ? Colors.grey : Colors.red,
+                      onPressed: () {
+                        setState(() {
+                          _likes[0] = !_likes[0];
+                        });
+                      },
+                    ),
                   ),
                 ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      //navigate to user messages
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => UserMessages()),
-                      );
-                    },
-                    child: Text('Message'),
+                Container(
+                  margin: EdgeInsets.only(left: 10, bottom: 15),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Description',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 10,
+                    bottom: 25,
+                  ),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '${widget.description}',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                // message input
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          maxLines: 10,
+                          decoration: InputDecoration(
+                            hintText: 'Hi, I\'m interested in this car!',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.send),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => UserMessages()),
+                          );
+                        },
+                      ),
+                      // IconButton(
+                      //   icon: Icon(Icons.send),
+                      //   onPressed: () {
+                      //     Navigator.push(
+                      //       context,
+                      //       MaterialPageRoute(
+                      //         builder: (context) => UserMessages(),
+                      //       ),
+                      //     );
+                      //   },
+                      // ),
+                    ],
                   ),
                 ),
               ],
-            ),
+            )
           ],
-        )));
+        ));
   }
 }
