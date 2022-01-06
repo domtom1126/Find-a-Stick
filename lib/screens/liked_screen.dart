8d9377ec56864a4241c54dd7458ff08aa3a189d8
dom thomas <domtom1126@gmail.com>
2022-01-05 23:23:25 -0500

    liked screen still needs functionality

--- lib/screens/liked_screen.dart
+++ lib/screens/liked_screen.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:find_stick/widgets/image_gallery.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class LikedScreen extends StatefulWidget {
   const LikedScreen({Key? key}) : super(key: key);
@@ -19,6 +20,13 @@ class _LikedScreenState extends State<LikedScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        bottom: PreferredSize(
+          child: Container(
+            color: HexColor('E07619'),
+            height: 2.0,
+          ),
+          preferredSize: Size.fromHeight(4.0),
+        ),
         title: Text('Liked'),
       ),
       // Todo This is where the liked widget should go
