2fd63689e77959b81dd87baddc89991622f44e34
dom thomas <domtom1126@gmail.com>
2022-01-09 22:59:41 -0500

    Once profile pic chosen, image uploads to firebase

--- lib/widgets/user_picked_image.dart
+++ lib/widgets/user_picked_image.dart
@@ -1,66 +1,51 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart' as path;
 
 Future<void> pickSingleImage() async {
   FirebaseStorage storageReference = FirebaseStorage.instance;
 
-  // List<XFile>? pickedImage = (await ImagePicker().pickMultiImage(
-  //   maxWidth: 100,
-  //   maxHeight: 100,
-  // ));
-  // XFile image = pickedImage![0];
-  // // * add images to firebase_storage after picking
-  // for (var i = 0; i < pickedImage!.length; i++) {
-  //   Reference storageReference = FirebaseStorage.instance
-  //       .ref()
-  //       .child('images/${pickedImage[i].path.split('/').last}');
-  //   File imageFile = File(pickedImage[i].path);
-  //   // * This is what uploads to firebase storage
-  //   // UploadTask uploadTask = storageReference.putFile(File(imageFile.path));
-  //   try {
-  //     await storageReference.putFile(imageFile);
-  //   } catch (e) {
-  //     print(e);
-  //   }
-  //   // * Downloads the URL and displays it in the carousel_slider
-  //   var imageUrl = await (await uploadTask).ref.getDownloadURL();
-  //   var url = imageUrl.toString();
-  //   setState(() {
-  //   // show image on screen after picking
-  //   imageList!.addAll([XFile(length: url)]);
-  //   });
-  // }
-  // return showDialog(
-  //     context: context,
-  //     builder: (context) {
-  //       return AlertDialog(
-  //         title: Text('Success'),
-  //         content: Text('Image uploaded'),
-  //         actions: <Widget>[
-  //           ElevatedButton(
-  //             child: Text('OK'),
-  //             onPressed: () {
-  //               Navigator.of(context).pop();
-  //             },
-  //           )
-  //         ],
-  //       );
-  //     });
-
   // select single image
   final picker = ImagePicker();
   try {
-    var pickedFile = await picker.pickImage(source: ImageSource.gallery);
-    final String fileName = pickedFile!.path;
-    print(fileName);
+    final List<XFile>? pickedFile = await picker.pickMultiImage();
+    for (final XFile file in pickedFile!) {
+      final String filePath = file.path;
+      storageReference.ref().child(filePath);
+      // await storageReference.ref().child(filePath).putFile(file);
+      // final TaskSnapshot downloadUrl = (await uploadTask);
+      // final String url = (await downloadUrl.ref.getDownloadURL());
+      // print(url);
+    }
+    // * The .ref refers to the things already inside of 'images'
+    // todo get downloadUrl for each image and store it in the post with the car
+    String downloadUrl = await storageReference.ref('images/').getDownloadURL();
+    print(downloadUrl);
 
-    // try {
-    //   await storageReference.putFile(imageFile);
-    // } catch (e) {
-    //   print(e);
-    // }
+    // print(pickedFile);
   } catch (e) {
     print(e);
   }
+}
+
+Future<void> uploadProfilePicture() async {
+  final picker = ImagePicker();
+  FirebaseStorage _storage = FirebaseStorage.instance;
+  final pickedFile = await picker.pickImage(
+    source: ImageSource.gallery,
+  );
+  // pickedFile needs to upload to firebase
+  XFile file = XFile(pickedFile!.path);
+  await _storage
+      .ref('/user_pictures')
+      .child(file.toString())
+      .putFile(File(pickedFile.path));
+
+  // get downloadUrl
+  // store downloadUrl in shared preferences
+  // todo upload image to firebase (shared preferences)
 }
